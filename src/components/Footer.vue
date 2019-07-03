30d8b10f79d5feafbd42370144ed171a21cac0a6
HSBL <amilhasbala@gmail.com>
2019-07-03 18:49:52 +0700

    try again

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer>
+    <p>&copy; 2019 Amil Hasbala</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Services.vue
@@ -0,0 +1,58 @@
+<template>
+  <section id="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="section-heading">What can i do for you?</h2>
+          <hr>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-sm-12 text-center">
+          <div class="service-box media">
+            <div class="media-left">
+              <div class="text-left">
+                <img
+                  src="../assets/icon-01.png"
+                  alt="design"
+                  class="img-responsive"
+                >
+              </div>
+            </div>
+            <div class="media-body text-left">
+              <h4>
+                <strong>Design What You Want.</strong>
+              </h4>
+              <p
+                class="text-muted line-height-2"
+              >I like to keep it simple. My goals are to focus on visual, content and conveying the message that you want to send.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-sm-12 text-center">
+          <div class="service-box media">
+            <div class="media-left">
+              <div class="text-left">
+                <img
+                  src="../assets/icon-02.png"
+                  alt="develop"
+                  class="img-responsive"
+                >
+              </div>
+            </div>
+            <div class="media-body text-left">
+              <h4>
+                <strong>Develop What You Need.</strong>
+              </h4>
+              <p
+                class="text-muted line-height-2"
+              >As a front end developer, I know how to create your website to run across devices using the latest technologies available.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
